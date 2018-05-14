42a9637e7acbf4539e226430c7df9059244feac0
Jackson Guo <jguo@CAN-JLC-ZeroGravity16.local>
2018-05-14 16:46:50 -0400

    updated packing format to wheel and added informative comments to setup.py

--- setup.py
+++ setup.py
@@ -1,5 +1,14 @@
 from setuptools import setup
 
+"""
+distutils - standard tool for packaging python projects, featured in the standard library 
+setuptools - heavily favoured third party tool for packaging python projects, not a part of the standard library 
+setup.py - canonical script used by both disutils and setuptools to gather the locations of project sources and metadata
+egg - python package format originally introduced by setuptools, it is essentially an importable zipped package 
+easy_install - python package manager introduced with setuptools which only installs egg packages from PyPI
+wheel - officially recommended package format for python projects, can be built with python3 setup.py bdist_wheel and installed with pip3 install dist/<name>.whl
+"""
+
 setup(
    name='slack_cli',
    version='1.0',
